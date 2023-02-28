62f7e2f0989946930d93f31c613104f8e253fd3d
IlyaSheklan <64840039+IlyaSheklan@users.noreply.github.com>
2023-02-28 17:25:59 +0700

    Delete constants directory

--- constants/constants.js
+++
@@ -1,5 +0,0 @@
-const btnSignUp = document.querySelector('.btn-sign-up');
-const btnLink = document.querySelector('.btn-log-in');
-const menu = document.querySelector('.menu');
-const authoBlock = document.querySelector('.content');
-const blockForError = document.querySelector('.block-for-error');
